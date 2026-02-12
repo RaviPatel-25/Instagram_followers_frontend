78b4fc28ce115a4da2a51bf771f445468bfbaf5b
Ravi <ramu1212patel8989@gmail.com>
2026-02-12 12:04:52 +0530

    ravi

---
+++ src/pages/AdminLogin.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {useNavigate} from 'react-router-dom'
+import axios from "axios";
+import BASE_URL from "../../config";
+
+export default function Login({ onLogin }) {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/api/admin/login`,
+        { email, password }
+      );
+
+      localStorage.setItem("adminToken", res.data.token);
+
+      onLogin();
+      navigate('/admindashboard');
+    } catch {
+      setError("Invalid Email or Password");
+    }
+  };
+
+  return (
+    <div className="login-page">
+
+      {/* Header */}
+      <div className="top-bar">
+        <h2>Instagram Followers</h2>
+        <span>Admin Panel</span>
+      </div>
+
+      {/* Login Card */}
+      <form className="login-card" onSubmit={handleSubmit}>
+
+        <h3>Admin Login</h3>
+
+        {error && <p className="error">{error}</p>}
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
+
+    </div>
+  );
+}
