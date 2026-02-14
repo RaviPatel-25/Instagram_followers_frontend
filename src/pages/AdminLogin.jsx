65710d5fbec3cbd3bf4382371855c02c388f8dc0
Ravi Patel <ravipatel.25.02.2005@gmail.com>
2026-02-14 09:27:33 +0530

    Update AdminLogin API endpoint to use external URL

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -14,7 +14,7 @@ export default function Login({ onLogin }) {
 
     try {
       const res = await axios.post(
-        "/api/admin/login",
+        "https://backendinsta-s0nf.onrender.com/api/admin/login",
         { email, password }
       );
 
